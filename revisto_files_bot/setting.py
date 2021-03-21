abc36d60246e486963f27f30fcfcd09754d85c7e
Revisto <walt.shabani@gmail.com>
2021-03-22 01:37:31 +0430

    add new paths for audio, video and pictures

--- revisto_files_bot/setting.py
+++ revisto_files_bot/setting.py
@@ -3,5 +3,8 @@ from os import getenv
 
 dotenv.load_dotenv()
 
-save_location = "/root/shared_data/"
+base_save_location = "/root/shared_data/telegram/"
+music_save_location = f"{base_save_location}musics/"
+video_save_location = f"{base_save_location}videos/"
+picture_save_location = f"{base_save_location}pictures/"
 telegram_access_token = getenv("telegram_robot_access_token")
